9aca04247be716cfd2059be945fdb857a7ef1737
SamarthBagga <samarthbagga@gmail.com>
2023-07-01 14:07:37 +0530

    gasReporter

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -2,11 +2,13 @@ require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
 require("./tasks/block-number")
 require("hardhat-gas-reporter")
+require("solidity-coverage")
 
 /** @type import('hardhat/config').HardhatUserConfig */
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || ""
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 
 module.exports = {
     defaultNetwork: "hardhat",
@@ -27,5 +29,9 @@ module.exports = {
     },
     gasReporter:{
         enabled:true,
+        outputFile:"gas-report.txt",
+        noColors:true,
+        currency:"USD",
+        coinmarketcap:COINMARKETCAP_API_KEY
     }
 }
