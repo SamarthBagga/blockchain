f4b59aea74585c366da3ad70ad7abb63ba1a0012
SamarthBagga <samarthbagga@gmail.com>
2023-07-03 14:58:07 +0530

    did stuff

--- hardhat-fundme/deploy/01-deploy-fund-me.js
+++ hardhat-fundme/deploy/01-deploy-fund-me.js
@@ -1,3 +1,28 @@
-function deployFunc() {}
+const { network } = require("hardhat")
+const { networkConfig, developmentChain } = require("../helper-hardhat-config")
 
-module.exports.default = deployFunc
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    let ethUSDPriceFeedAddress
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUSDPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        const ethUSDPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+
+    //when going for localhost or hardhat network we want to use a mock
+
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [], // put price feed address
+        log: true,
+    })
+    log("------------------------------------------------------------------------------")
+
+}
+
+module.exports.tags = ["ethUsdPriceFeedAddress"]
