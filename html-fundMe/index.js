9c40a33ba1c47f14abec6c4038ea5f9f8953a604
SamarthBagga <samarthbagga@gmail.com>
2023-07-12 21:33:14 +0530

    html fundme

---
+++ html-fundMe/index.js
@@ -0,0 +1,49 @@
+import { ethers } from "./ethers-5.6.esm.min.js"
+import { abi, contractAddress } from "./constants.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fund")
+connectButton.onclick = connect
+fundButton.onclick = fund
+async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+        window.ethereum.request({ method: "eth_requestAccounts" })
+        connectButton.innerHTML = "Connected!"
+    } else {
+        connectButton.innerHTML = "Please install Metamask"
+    }
+}
+
+async function fund(ethAmount) {
+    console.log(`Funding with ${ethAmount}`)
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.fund({
+                value: ethers.utils.parseEther(ethAmount),
+            })
+            // listen for the tx to be mined
+            await listenForTransactionMine(transactionResponse, provider)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+    console.log(`Mining ${transactionResponse.hash}...`)
+    // return new Promise()
+    // create a listener for the blockchain
+    return new Promise((resolve, reject) => {
+        provider.once(transactionResponse.hash, (transactionReceipt) => {
+            console.log(
+                `completed with ${transactionReceipt.confirmations} confirmations`,
+            )
+            resolve()
+        })
+    })
+    
+
+}
