8106a295f850fc8d993464f2279f50407f66a21a
SamarthBagga <samarthbagga@gmail.com>
2023-07-14 12:37:39 +0530

    html fund me completed

--- html-fundMe/index.js
+++ html-fundMe/index.js
@@ -2,9 +2,15 @@ import { ethers } from "./ethers-5.6.esm.min.js"
 import { abi, contractAddress } from "./constants.js"
 
 const connectButton = document.getElementById("connectButton")
-const fundButton = document.getElementById("fund")
+const fundButton = document.getElementById("fundButton")
+const balanceButton = document.getElementById("balanceButton")
+const withdrawButton = document.getElementById("withdrawButton")
+
 connectButton.onclick = connect
 fundButton.onclick = fund
+balanceButton.onclick = getBalance
+withdrawButton.onclick = withdraw
+
 async function connect() {
     if (typeof window.ethereum !== "undefined") {
         window.ethereum.request({ method: "eth_requestAccounts" })
@@ -13,8 +19,14 @@ async function connect() {
         connectButton.innerHTML = "Please install Metamask"
     }
 }
-
-async function fund(ethAmount) {
+async function getBalance() {
+    if (typeof window.ethereum != "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const balance = await provider.getBalance(contractAddress)
+        console.log(ethers.utils.formatEther(balance))
+    }
+}
+async function fund() {
     const ethAmount = document.getElementById("ethAmount").value
     console.log(`Funding with ${ethAmount}`)
     if (typeof window.ethereum !== "undefined") {
@@ -45,6 +57,20 @@ function listenForTransactionMine(transactionResponse, provider) {
             resolve()
         })
     })
-    
+}
 
+async function withdraw(){
+    if (typeof window.ethereum != "undefined") {
+        console.log("Withdrawing....")
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress,abi,signer)
+        try{
+            const transactionResponse = await contract.withdraw()
+            await listenForTransactionMine(transactionResponse, provider)
+        }catch(error){
+            console.log(error)
+        }
+
+    }
 }
