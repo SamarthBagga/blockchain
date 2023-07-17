40ffe89d67014c18d369225e399a54e86a633fe6
SamarthBagga <samarthbagga@gmail.com>
2023-07-17 18:11:24 +0530

    setup for raffle

--- hardhat-smartcontract-lottery/deploy/00-deploy-mocks.js
+++ hardhat-smartcontract-lottery/deploy/00-deploy-mocks.js
@@ -1,8 +1,26 @@
 const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers")
+
+const BASE_FEE = ethers.utils.parseEther("0.25")
+const GAS_PRICE_LINK = 1e9 //calculated value based on the gas price of
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
-    const chainId = network.config.chainId
-    
+    const args = [BASE_FEE, GAS_PRICE_LINK]
+
+    if (developmentChains.includes(network.name)) {
+        log("local network detected! deploying mocks")
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            log: true,
+            args: args,
+        })
+        log("Mocks deployed")
+        loadFixture("--------------------------------")
+    }
 }
+
+
+module.exports.tags = ["all", "mocks" ]
--- hardhat-smartcontract-lottery/helper-hardhat-config.js
+++ hardhat-smartcontract-lottery/helper-hardhat-config.js
@@ -1,7 +1,20 @@
+const { ethers } = require("hardhat")
+
 const networkConfig = {
-    4: {
+    111555111: {
         name: "sepolia",
         vrfCoordinatorV2: "0x271682DEB8C4E0901D1a1550aD2e64D568E69909",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+        subscriptionId: "0",
+        callbackGasLimit: "500000",
+        interval:"30",
+    },
+    31337: {
+        name:"hardhat",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane:"0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
+        interval:"30",
     },
 }
 
