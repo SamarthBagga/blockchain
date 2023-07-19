2c7f378c839e563c320ee0ab6d1f8deb28cbe0fd
SamarthBagga <samarthbagga@gmail.com>
2023-07-19 16:38:20 +0530

    did stuff

--- hardhat-smartcontract-lottery/helper-hardhat-config.js
+++ hardhat-smartcontract-lottery/helper-hardhat-config.js
@@ -1,20 +1,26 @@
 const { ethers } = require("hardhat")
 
 const networkConfig = {
+    default: {
+        name: "hardhat",
+        keepersUpdateInterval: "30",
+    },
     111555111: {
         name: "sepolia",
         vrfCoordinatorV2: "0x271682DEB8C4E0901D1a1550aD2e64D568E69909",
-        entranceFee: ethers.parseEther("0.01"),
+        raffleEntranceFee: ethers.parseEther("0.01"),
         gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
         subscriptionId: "0",
         callbackGasLimit: "500000",
-        interval:"30",
+        keepersUpdateInterval:"30",
     },
     31337: {
         name:"hardhat",
-        entranceFee: ethers.parseEther("0.01"),
+        subscriptionId: "6926",
+        raffleEntranceFee: ethers.parseEther("0.01"),
         gasLane:"0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
-        interval:"30",
+        keepersUpdateInterval:"30",
+        callbackGasLimit: "500000",
     },
 }
 
