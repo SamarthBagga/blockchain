dcc7919311fadb8f0a5487705c5f078f6afdaa84
SamarthBagga <samarthbagga@gmail.com>
2023-08-22 22:40:29 +0530

    added

--- hardhat-smartcontract-lottery/helper-hardhat-config.js
+++ hardhat-smartcontract-lottery/helper-hardhat-config.js
@@ -10,8 +10,8 @@ const networkConfig = {
         vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
         raffleEntranceFee: ethers.parseEther("0.01"),
         gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
-        subscriptionId: "4541",
-        callbackGasLimit: "500000",
+        subscriptionId: "4675",
+        callbackGasLimit: "200000",
         keepersUpdateInterval: "30",
     },
     31337: {
