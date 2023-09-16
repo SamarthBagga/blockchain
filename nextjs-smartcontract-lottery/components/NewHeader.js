406be4d57edf1fa5adf77493a1a740edf34bb3b1
SamarthBagga <samarthbagga@gmail.com>
2023-09-16 17:21:36 +0530

    lottery completed

--- nextjs-smartcontract-lottery/components/NewHeader.js
+++ nextjs-smartcontract-lottery/components/NewHeader.js
@@ -1,5 +1,12 @@
 import { ConnectButton } from "web3uikit"
 
 export default function NewHeader() {
-    return <ConnectButton moralisAuth={false} />
+    return (
+        <div className="border-b-2 p-5 flex flex-row">
+            <h1 className="py-4 px-4 font-bold text-3xl">Dececntralized Lottery</h1>
+            <div className="ml-auto py-2 px-2">
+                <ConnectButton moralisAuth={false} />
+            </div>
+        </div>
+    )
 }
--- nextjs-smartcontract-lottery/src/pages/index.js
+++ nextjs-smartcontract-lottery/src/pages/index.js
@@ -16,7 +16,6 @@ export default function Home() {
             </Head>
             <NewHeader />
             <LotteryEntrance/>
-            Hello!
         </>
     )
 }
