89cda522c97eacb9fd675d29e06e18918c690344
SamarthBagga <samarthbagga@gmail.com>
2023-09-26 08:13:51 +0530

    did stuff

--- hardhat-aave/hardhat.config.js
+++ hardhat-aave/hardhat.config.js
@@ -9,7 +9,7 @@ require("hardhat-deploy")
 
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
-const PRIVATE_KEY = 
+const PRIVATE_KEY =
     process.env.PRIVATE_KEY ||
     "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
@@ -38,7 +38,7 @@ module.exports = {
                 version: "0.8.7",
             },
             {
-                version: "0.6.6",
+                version: "0.4.19",
             },
         ],
     },
