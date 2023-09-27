b248462d840f81268b8a7d039ea9860d0c704245
SamarthBagga <samarthbagga@gmail.com>
2023-09-27 12:21:26 +0530

    got aave address

--- hardhat-aave/scripts/aaveBorrow.js
+++ hardhat-aave/scripts/aaveBorrow.js
@@ -1,32 +1,38 @@
-const { getNamedAccounts, ethers } = require("hardhat")
-const { getWeth } = require("../scripts/getWeth")
-async function main() {
-    await getWeth()
-    const { deployer } = await getNamedAccounts()
-    const lendingPool = await getLendingPool(deployer)
-    console.log(`LendingPool address ${lendingPool.address}`)
-    // we need the abi and the address
+const { getNamedAccounts, ethers } = require('hardhat');
+const { getWeth } = require('./getWeth');
 
-    // 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+async function main() {
+	// the protocol treats everything as an ERC-20 token
+	await getWeth();
+	const { deployer } = await getNamedAccounts();
+	const signer = await ethers.provider.getSigner();
+	// Lending Pool Address Provider: 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+	const lendingPool = await getLendingPool(signer);
+	const lendingPoolContractAddress = await lendingPool.getAddress();
+	console.log(`Lending Pool address ${lendingPoolContractAddress}`);
+
+    // time to depositt!!!!
 }
 
 async function getLendingPool(account) {
-    const lendingPoolAddressProvider = await ethers.getContractAt(
-        "ILendingPoolAddressesProvider",
-        "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
-        account
-    )
-    const lendingPoolAddress = await lendingPoolAddressProvider.getLendingPool()
-    const lendingPool = await ethers.getContractAt(
-        "ILendingPool",
-        lendingPoolAddress,
-        account
-    )
+	const lendingPoolAddressesProvider = await ethers.getContractAt(
+		'ILendingPoolAddressesProvider',
+		'0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5',
+		account
+	);
+	const lendingPoolAddress =
+		await lendingPoolAddressesProvider.getLendingPool();
+	const lendingPool = await ethers.getContractAt(
+		'ILendingPool',
+		lendingPoolAddress,
+		account
+	);
+	return lendingPool;
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error)
-        process.exit(1)
-    })
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
